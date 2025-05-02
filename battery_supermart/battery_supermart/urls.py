e37d30009bcf4d03fabd7e57adde14f5edcc2f46
Mishthi27 <mishthisachdeva27@gmail.com>
2025-05-03 03:25:50 +0530

    Update urls.py

--- battery_supermart/battery_supermart/urls.py
+++ battery_supermart/battery_supermart/urls.py
@@ -1,13 +1,23 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('store.urls')),  # Your store app routes
 ]
 
-# Serve static and media files
+# Serve static files (e.g. CSS, JS)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# Serve media files (e.g. uploaded product images)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+    ]
